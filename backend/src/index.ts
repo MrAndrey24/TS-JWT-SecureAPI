87bd9b2ee45fd950dd94c51938f29fcf21b7a3b9
Andrey Acosta  R <andreyacostar@gmail.com>
2023-10-04 16:06:57 -0600

    user and task routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { env } from 'process';
+import userRouter from './routes/user';
+import taskRouter from './routes/task';
+
 
 const app = express();
 app.use(express.json());
+
+// Routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tasks', taskRouter);
 
 
 const port = env.PORT || 3000;
